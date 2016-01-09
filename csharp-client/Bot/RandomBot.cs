89ec6da1f887a673501b0def5b4e80a330aee5a3
Mathieu Gamache <mathg99@hotmail.com>
2016-01-09 10:00:19 -0500

    begining of a*

--- csharp-client/Bot/RandomBot.cs
+++ csharp-client/Bot/RandomBot.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) 2005-2016, Coveo Solutions Inc.
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using Coveo.Bot;
 
 namespace CoveoBlitz.RandomBot
 {
@@ -28,33 +33,135 @@ namespace CoveoBlitz.RandomBot
         /// <returns></returns>
         public string Move(GameState state)
         {
-            string direction;
 
-            switch (random.Next(0, 5)) {
-                case 0:
-                    direction = Direction.East;
-                    break;
+        }
 
-                case 1:
-                    direction = Direction.West;
-                    break;
+        private string closesPath(GameState state, Pos start, Pos goal)
+        {
+            List<PathCoord> visited = new List<PathCoord>();
+            List<PathCoord> availableTiles = new List<PathCoord>();
 
-                case 2:
-                    direction = Direction.North;
-                    break;
+            var first = createPathCoord(start, null, state, goal);
+            visited.Add(first);
+            availableTiles.Add(first);
 
-                case 3:
-                    direction = Direction.South;
-                    break;
+            while (availableTiles.Any())
+            {
+                // Sort availableTiles
+                availableTiles.Sort((f1, f2) => f1.heuristic.CompareTo(f2.heuristic));
 
-                default:
-                    direction = Direction.Stay;
-                    break;
+                var next = availableTiles.First(); // todo sort by heuristic
+
+                if (next.current == goal)
+                {
+                    return Restitute(start, next);
+                }
+                else
+                {
+                    var spreadFrom = 
+                }
+            }
+        }
+
+        private string Restitute(Pos start, PathCoord foundPath)
+        {
+
+        }
+
+
+        private List<PathCoord> getAvailableCoords(PathCoord current, GameState state, Pos goal)
+        {
+            PathCoord up = createPathCoord(new Pos() {x = current.current.x, y = current.current.y + 1}, current, state,
+                goal);
+            PathCoord down = createPathCoord(new Pos() { x = current.current.x, y = current.current.y - 1 }, current, state,
+                goal);
+            PathCoord left = createPathCoord(new Pos() { x = current.current.x - 1, y = current.current.y }, current, state,
+                goal);
+            PathCoord right = createPathCoord(new Pos() { x = current.current.x + 1, y = current.current.y }, current, state,
+                goal);
+
+            var rc = new List<PathCoord>();
+            if (up != null)
+            {
+                rc.Add(up);
+            }
+            if (down != null)
+            {
+                rc.Add(down);
+            }
+            if (left != null)
+            {
+                rc.Add(left);
+            }
+            if (right != null)
+            {
+                rc.Add(right);
+            }
+            return rc;
+        } 
+
+        private PathCoord createPathCoord(Pos current, PathCoord previous, GameState state, Pos goal)
+        {
+            PathCoord pc = new PathCoord();
+
+            if (state.board[current.x][current.y] == Tile.IMPASSABLE_WOOD)
+            {
+                return null;
             }
 
-            Console.WriteLine("Completed turn {0}, going {1}", state.currentTurn, direction);
-            return direction;
+            if (previous != null)
+            {
+                pc.weight = previous.weight; // to modify
+            }
+
+            if (state.board[current.x][current.y] == Tile.SPIKES)
+            {
+                pc.weight += 3; // to modify
+            }
+            else
+            {
+                pc.weight += 1;
+            }
+
+            pc.heuristic = pc.weight + getDistance(current, goal);
+            return pc;
         }
+
+        private int getDistance(Pos a, Pos b)
+        {
+            return Math.Abs(b.x - a.x) + Math.Abs(b.y - a.y);
+        }
+
+        //public string Move(GameState state)
+        //{
+        //    string direction;
+
+        //    switch (random.Next(0, 5))
+        //    {
+        //        case 0:
+        //            direction = Direction.East;
+        //            break;
+
+        //        case 1:
+        //            direction = Direction.West;
+        //            break;
+
+        //        case 2:
+        //            direction = Direction.North;
+        //            break;
+
+        //        case 3:
+        //            direction = Direction.South;
+        //            break;
+
+        //        default:
+        //            direction = Direction.Stay;
+        //            break;
+        //    }
+
+        //    Console.WriteLine("Completed turn {0}, going {1}", state.currentTurn, direction);
+        //    return direction;
+        //}
 
         /// <summary>
         /// This is run after the game.
